7c5ac7a72615ffb64084d552c6fedd81f3d118c7
Juan Antonio Fernández de Alba <juan@undefinedlabs.com>
2019-11-29 16:22:37 +0100

    add action

---
+++ src/index.js
@@ -0,0 +1,51 @@
+const core = require('@actions/core')
+const exec = require('@actions/exec')
+
+const SCOPE_DSN = 'SCOPE_DSN'
+
+async function run() {
+  try {
+    const command = core.getInput('command') || 'npm test'
+    const dsn = core.getInput('dsn') || process.env[SCOPE_DSN]
+
+    if (!dsn) {
+      throw Error('Cannot find the Scope DSN')
+    }
+
+    let apiEndpoint, apiKey
+    try {
+      const { username, origin } = new URL(dsn)
+      apiEndpoint = origin
+      apiKey = username
+    } catch (e) {}
+
+    console.log(`Command: ${command}`)
+    if (dsn) {
+      console.log(`DSN has been set.`)
+    }
+
+    await exec.exec('npm install --save-dev @undefinedlabs/scope-agent', null, {
+      ignoreReturnCode: true,
+    })
+
+    await ExecScopeRun(command, apiEndpoint, apiKey)
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+const DEFAULT_COMMAND =
+  'npm test --testRunner=@undefinedlabs/scope-agent/jestTestRunner --runner=@undefinedlabs/scope-agent/jestRunner --setupFilesAfterEnv=@undefinedlabs/scope-agent/jestSetupTests'
+
+function ExecScopeRun(command = DEFAULT_COMMAND, apiEndpoint, apiKey) {
+  return exec.exec(command, null, {
+    env: {
+      ...process.env,
+      SCOPE_API_ENDPOINT: apiEndpoint,
+      SCOPE_APIKEY: apiKey,
+      SCOPE_AUTO_INSTRUMENT: true,
+    },
+  })
+}
+
+run()
